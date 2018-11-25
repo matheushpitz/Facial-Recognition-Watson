3c8de72f5446ca6387129e3b8978c2f6e9cd56fa
Matheus Dell <matheushenriquepitz@gmail.com>
2018-11-25 20:11:08 -0200

    Adding the face's renderization.

--- client/src/facial-recognition/components/ImageUploader.js
+++ client/src/facial-recognition/components/ImageUploader.js
@@ -29,12 +29,32 @@ class ImageUploader extends React.Component {
         }
     }
 
+    renderFaces() {
+        // Checking if the data is valid.
+        if(this.props.data == undefined)
+            return undefined;
+        // Render the faces
+        return (
+            <div>
+                {this.props.data.images[0].faces.map( (face, idx) => {
+                    let style = face['face_location'];
+                    return (
+                        <div className = "face-box" key={idx} style = {style}>
+                            <p className = "face-name">Face {idx}</p>
+                        </div>
+                    );
+                })}
+            </div>
+        );
+    }
+
     render() {
         return(
             <div className = "image-uploader">
                 <img src={this.state.img} alt = {'Facial recognition'} />
                 <input type = {'file'} style = {{display: 'none'}} ref = {this.fileLoader} onChange = {this.changeFileHandle} />
                 <button onClick = { () => this.fileLoader.current.click() }>Upload</button>
+                {this.renderFaces()}
             </div>
         );
     }
--- client/src/facial-recognition/index.js
+++ client/src/facial-recognition/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import Axios from 'axios';
 import PropTypes from 'prop-types';
 import ImageUploader from './components/ImageUploader';
+import './style.css';
 
 class FacialRecognition extends React.Component {
     constructor(props) {
         super(props);
+        // Initializing the state
+        this.state = {
+            response: undefined
+        };
         // Creating an instance of Axios with all the configs.
         this.axios = Axios.create({
             baseURL: props.serviceDomain,
@@ -19,10 +24,14 @@ class FacialRecognition extends React.Component {
     }
 
     changeHandle(img) {
+        // Before make the request, let's clear the response
+        this.setState({response: undefined});
         // make a POST requests to the server.
         this.axios.post('/', {image: img}).then( (res) => {
-            // Success
-            alert('Receiving the response -> '+res.data);
+            // Set the state with the response.
+            this.setState({
+                response: res.data
+            });
         }).catch( (err) => {
             // Fail
             alert('Server communication error. '+err);
@@ -32,7 +41,7 @@ class FacialRecognition extends React.Component {
     render() {
         return(
             <div className = "facial-recognition">
-                <ImageUploader onChange = {this.changeHandle}  />
+                <ImageUploader onChange = {this.changeHandle} data = {this.state.response} />
             </div>
         );
     }
